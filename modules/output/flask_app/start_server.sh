df1c32aae27d5b0f5f918733d0475c39b2add890
Satria A Kautsar <satriaphd@gmail.com>
2020-05-11 16:56:56 +0200

    feature: added the ability to set port for output viz

--- modules/output/flask_app/start_server.sh
+++ modules/output/flask_app/start_server.sh
@@ -4,4 +4,10 @@ DIR="$(dirname "$(readlink -f "$0")")"
 export FLASK_APP="$DIR/app/run.py"
 
 # run flask server
-flask run
+if [ -z "$1" ]
+then
+  port="5000"
+else
+  port=$1
+fi
+flask run --host=0.0.0.0 --port=$port
