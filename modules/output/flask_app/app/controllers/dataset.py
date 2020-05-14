16bcb2d4043256382f02979139afbaf89e8ec9bf
Satria Kautsar <satriaphd@gmail.com>
2020-05-14 08:03:01 +0200

    ui: implements 'browse' datatable in dataset page

--- modules/output/flask_app/app/controllers/dataset.py
+++ modules/output/flask_app/app/controllers/dataset.py
@@ -3,6 +3,7 @@
 import sqlite3
 from flask import render_template, request
 import json
+from os import path
 
 # import global config
 from ..config import conf
@@ -53,6 +54,7 @@ def get_bgc_table():
 
     # translate request parameters
     dataset_id = request.args.get('dataset_id', default=0, type=int)
+    result["dataset_id"] = dataset_id
     offset = request.args.get('start', type=int)
     limit = request.args.get('length', type=int)
 
@@ -75,16 +77,28 @@ def get_bgc_table():
         ).format("=" if dataset_id > 0 else "!="),
             (dataset_id,)).fetchall()[0][0]
 
+        # fetch taxonomy descriptor
+        result["taxon_desc"] = cur.execute((
+            "select id, name"
+            " from taxon_class"
+            " order by level asc"
+        )).fetchall()
+
         # fetch data for table
         result["data"] = []
         for row in cur.execute((
-            "select id,orig_folder,name,length_nt,on_contig_edge"
+            "select id as bgc_id"
+            ",orig_folder as genome"
+            ",orig_filename as bgc_name"
+            ",length_nt,on_contig_edge"
             " from bgc"
             " where dataset_id{}?"
             " limit ? offset ?"
         ).format("=" if dataset_id > 0 else "!="),
                 (dataset_id, limit, offset)).fetchall():
             bgc_id, genome, name, length, fragmented = row
+
+            # fetch basic data
             data = {
                 "bgc_id": bgc_id,
                 "genome_name": genome if genome else "n/a",
@@ -97,17 +111,41 @@ def get_bgc_table():
                 data["completeness"] = "complete"
             else:
                 data["completeness"] = "n/a"
-            data["taxon_name"] = ""
-            data["gene_counts"] = ""
-            data["class_name"] = ""
+
+            # fetch genes count
+            data["genes_count"] = cur.execute((
+                "select count(id)"
+                " from cds"
+                " where bgc_id=?"
+            ), (bgc_id, )).fetchall()[0][0]
+
+            # fetch taxonomy information
+            data["taxonomy"] = {
+                row[0]: row[1] for row in cur.execute((
+                    "select taxon_id, taxon.name"
+                    " from taxon, bgc_taxonomy"
+                    " where taxon.id=bgc_taxonomy.taxon_id"
+                    " and bgc_taxonomy.bgc_id=?"
+                ), (bgc_id, )).fetchall()
+            }
+
+            # fetch class information
+            data["class_name"] = cur.execute((
+                "select chem_class.name, chem_subclass.name"
+                " from bgc, bgc_class, chem_subclass, chem_class"
+                " where bgc.id=bgc_class.bgc_id"
+                " and bgc_class.chem_subclass_id=chem_subclass.id"
+                " and chem_subclass.class_id=chem_class.id"
+                " and bgc.id=?"
+            ), (bgc_id, )).fetchall()
 
             result["data"].append([
                 data["genome_name"],
                 data["bgc_name"],
-                data["taxon_name"],
+                data["taxonomy"],
                 data["class_name"],
                 data["bgc_length"],
-                data["gene_counts"],
+                data["genes_count"],
                 data["completeness"],
                 data["bgc_id"]
             ])
