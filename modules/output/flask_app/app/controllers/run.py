02deb6e9e2d74ba9c9300308783dcc4c07bc8654
Satria Kautsar <satriaphd@gmail.com>
2020-05-14 18:03:58 +0200

    ui: implement ui for Runs

--- modules/output/flask_app/app/controllers/run.py
+++ modules/output/flask_app/app/controllers/run.py
@@ -14,17 +14,30 @@ blueprint = Blueprint('run', __name__)
 @blueprint.route("/run/<int:run_id>")
 def page_run(run_id):
 
-    # todo: implements
-
     # page title
-    page_title = "Run"
-    page_subtitle = (
-        "under construction"
-    )
+    with sqlite3.connect(conf["db_path"]) as con:
+        cur = con.cursor()
+        status_id, = cur.execute((
+            "select status"
+            " from run"
+            " where id = ?"
+        ), (run_id, )).fetchall()[0]
+
+        page_title = "Run-{:04d}".format(run_id)
+        page_subtitle = ("Here you can find the complete summary "
+                         "generated for this run. When a run "
+                         "finishes, you can view the general "
+                         "statistics of the run and browse through "
+                         "all the GCFs and their BGC members assigned"
+                         " by the algorithm. Click 'view' on the "
+                         "data table to see the detailed summary page "
+                         "of each GCF.")
 
     # render view
     return render_template(
         "run/main.html.j2",
+        run_id=run_id,
+        status_id=status_id,
         page_title=page_title,
         page_subtitle=page_subtitle
     )
