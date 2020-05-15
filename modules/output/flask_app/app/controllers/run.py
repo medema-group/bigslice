11ab58112cf4bfc4f773a6d07c321e2151b4d932
Satria Kautsar <satriaphd@gmail.com>
2020-05-15 06:33:34 +0200

    ui: implements overview block on "Runs" page

--- modules/output/flask_app/app/controllers/run.py
+++ modules/output/flask_app/app/controllers/run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sqlite3
-from flask import render_template
+from flask import render_template, request
 
 # import global config
 from ..config import conf
@@ -41,3 +41,90 @@ def page_run(run_id):
         page_title=page_title,
         page_subtitle=page_subtitle
     )
+
+
+@blueprint.route("/api/run/get_overview")
+def get_overview():
+    """ for statistic summary """
+    run_id = request.args.get('run_id', type=int)
+    result = {}
+    with sqlite3.connect(conf["db_path"]) as con:
+        cur = con.cursor()
+
+        # get status, parameters, hmm_db_id
+        result["status"], result["parameters"], hmm_db_id = cur.execute((
+            "select enum_run_status.name, run.prog_params, run.hmm_db_id"
+            " from run, enum_run_status"
+            " where enum_run_status.id=run.status"
+            " and run.id = ?"
+        ), (run_id, )).fetchall()[0]
+
+        # fetch start time
+        try:
+            result["started"] = cur.execute(
+                ("select strftime('%Y-%m-%d %H:%M:%S', time_stamp)"
+                    " from run_log"
+                    " where run_id=? and message like 'run created %'"),
+                (run_id, )
+            ).fetchall()[0][0]
+        except IndexError:
+            result["start"] = "n/a"
+
+        # fetch end time
+        try:
+            result["finished"] = cur.execute(
+                ("select strftime('%Y-%m-%d %H:%M:%S', time_stamp)"
+                    " from run_log"
+                    " where run_id=? and message like 'run finished'"),
+                (run_id, )
+            ).fetchall()[0][0]
+        except IndexError:
+            result["finished"] = "n/a"
+
+        # fetch resumes
+        result["resumes"] = [row[0] for row in cur.execute(
+            ("select strftime('%Y-%m-%d %H:%M:%S', time_stamp)"
+                " from run_log"
+                " where run_id=? and message like 'run resumed %'"),
+            (run_id, )
+        ).fetchall()]
+
+        # get hmm counts
+        result["hmm_counts"] = cur.execute(
+            ("select count(id)"
+             " from hmm"
+             " where db_id=?"),
+            (hmm_db_id, )
+        ).fetchall()[0][0]
+        result["hmm_subpfam_counts"] = cur.execute(
+            ("select count(id)"
+             " from hmm,subpfam"
+             " where db_id=?"
+             " and subpfam.hmm_id=hmm.id"),
+            (hmm_db_id, )
+        ).fetchall()[0][0]
+        result["hmm_core_counts"] = cur.execute(
+            ("select count(distinct id)"
+             " from hmm,subpfam"
+             " where db_id=?"
+             " and subpfam.parent_hmm_id=hmm.id"),
+            (hmm_db_id, )
+        ).fetchall()[0][0]
+
+        # get bgc/dataset counts
+        result["bgc_counts_per_dataset"] = {
+            row[0]: row[1] for row in cur.execute(
+                ("select dataset.name, count(bgc.id)"
+                 " from bgc, dataset, run_bgc_status"
+                 " where bgc.id=run_bgc_status.bgc_id"
+                 " and dataset.id=bgc.dataset_id"
+                 " and run_bgc_status.run_id=?"
+                 " group by dataset.name"),
+                (run_id, )
+            ).fetchall()
+        }
+        result["bgc_counts_total"] = sum(
+            result["bgc_counts_per_dataset"].values())
+        result["dataset_counts"] = len(result["bgc_counts_per_dataset"])
+
+    return result
