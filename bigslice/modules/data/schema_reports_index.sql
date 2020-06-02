d281a8a049b1a40828334829406a1e465369b41d
Satria A Kautsar <satriaphd@gmail.com>
2020-06-02 18:08:57 +0200

    core: store run id in reports index

--- bigslice/modules/data/schema_reports_index.sql
+++ bigslice/modules/data/schema_reports_index.sql
@@ -3,10 +3,18 @@ CREATE TABLE IF NOT EXISTS reports (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     type VARCHAR(250) NOT NULL,
     name VARCHAR(250) NOT NULL,
-    creation_date DATETIME NOT NULL, 
+    creation_date DATETIME NOT NULL,
     UNIQUE(type, name)
 );
 CREATE INDEX IF NOT EXISTS reports_type_name ON reports(type, name);
 CREATE INDEX IF NOT EXISTS reports_type_date ON reports(type, creation_date);
 CREATE INDEX IF NOT EXISTS reports_date ON reports(creation_date);
 CREATE INDEX IF NOT EXISTS reports_name ON reports(name);
+
+CREATE TABLE IF NOT EXISTS reports_run (
+    report_id INTEGER NOT NULL,
+    run_id INTEGER NOT NULL,
+    UNIQUE(report_id, run_id)
+);
+CREATE INDEX IF NOT EXISTS reports_run_report ON reports_run(report_id, run_id);
+CREATE INDEX IF NOT EXISTS reports_run_run ON reports_run(run_id, report_id);
