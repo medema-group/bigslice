4e93611de6e5f06730778f5d6e12f6d568155633
Satria A Kautsar <satriaphd@gmail.com>
2020-06-03 20:23:01 +0200

    ui: implements query detail page (1 of 2)

--- bigslice/modules/output/flask_app/app/controllers/reports/query.py
+++ bigslice/modules/output/flask_app/app/controllers/reports/query.py
@@ -61,32 +61,43 @@ def page_query_detail(report_id, bgc_id):
         cur = con.cursor()
         with sqlite3.connect(conf["db_path"]) as con_source:
             cur_source = con_source.cursor()
+            db_query_path = path.join(
+                conf["reports_folder"], str(report_id), "data.db")
+            with sqlite3.connect(db_query_path) as con_query:
+                cur_query = con_query.cursor()
 
-            name, created = cur.execute((
-                "select name, strftime('%Y-%m-%d %H:%M:%S', creation_date)"
-                " from reports"
-                " where id=?"
-                " and type=?"
-            ), (report_id, "query")).fetchall()[0]
+                name, created = cur.execute((
+                    "select name, strftime('%Y-%m-%d %H:%M:%S', creation_date)"
+                    " from reports"
+                    " where id=?"
+                    " and type=?"
+                ), (report_id, "query")).fetchall()[0]
 
-            # set run_id
-            run_id = cur.execute((
-                "select run_id"
-                " from reports_run"
-                " where report_id=?"
-            ), (report_id, )).fetchall()[0][0]
+                # set run_id
+                run_id = cur.execute((
+                    "select run_id"
+                    " from reports_run"
+                    " where report_id=?"
+                ), (report_id, )).fetchall()[0][0]
 
-            # set clustering id, get threshold
-            clustering_id, threshold = cur_source.execute(
-                ("select id, threshold"
-                 " from clustering"
-                 " where run_id=?"),
-                (run_id, )
-            ).fetchall()[0]
+                # fetch bgc name
+                bgc_name = cur_query.execute((
+                    "select name"
+                    " from bgc"
+                    " where id=?"
+                ), (bgc_id, )).fetchall()[0][0]
+
+                # set clustering id, get threshold
+                clustering_id, threshold = cur_source.execute(
+                    ("select id, threshold"
+                     " from clustering"
+                     " where run_id=?"),
+                    (run_id, )
+                ).fetchall()[0]
 
     # page title
-    page_title = "Query result: {} (BGC: {})".format(name, bgc_id)
-    page_subtitle = "(under construction)"
+    page_title = "Query result: {}".format(bgc_name)
+    page_subtitle = "From query: '{}'".format(name)
 
     # render view
     return render_template(
@@ -95,9 +106,399 @@ def page_query_detail(report_id, bgc_id):
         page_subtitle=page_subtitle,
         bgc_id=bgc_id,
         run_id=run_id,
+        report_id=report_id,
         clustering_id=clustering_id,
         threshold=threshold
     )
+
+
+def detail_get_overview():
+    """ for bgc overview tables """
+    result = {}
+    bgc_id = request.args.get('bgc_id', type=int)
+    report_id = request.args.get('report_id', type=int)
+    run_id = request.args.get('run_id', type=int)
+    result["run_id"] = run_id
+    result["run_name"] = "run-{:04d}".format(run_id)
+    db_query_path = path.join(
+        conf["reports_folder"], str(report_id), "data.db")
+
+    with sqlite3.connect(conf["reports_db_path"]) as con:
+        cur = con.cursor()
+        with sqlite3.connect(conf["db_path"]) as con_source:
+            cur_source = con_source.cursor()
+            with sqlite3.connect(db_query_path) as con_query:
+                cur_query = con_query.cursor()
+
+                # fetch query created
+                result["run_created"] = cur.execute((
+                    "select strftime('%Y-%m-%d %H:%M:%S', creation_date)"
+                    " from reports"
+                    " where id=?"
+                ), (report_id, )).fetchall()[0][0]
+
+                # fetch direct properties
+                (folder_path, file_path, on_contig_edge,
+                 result["length"], result["type"]) = cur_query.execute((
+                     "select orig_folder, orig_filename,"
+                     " on_contig_edge, length_nt, type"
+                     " from bgc"
+                     " where bgc.id=?"
+                 ), (bgc_id, )).fetchall()[0]
+                result["file_path"] = path.join("/", folder_path, file_path)
+                result["fragmented"] = "yes" if on_contig_edge == 1 else (
+                    "no" if on_contig_edge == 0 else "n/a"
+                )
+
+                # fetch type desc
+                result["type_desc"] = cur_source.execute((
+                    "select description"
+                    " from enum_bgc_type"
+                    " where code=?"
+                ), (result["type"], )).fetchall()[0][0]
+
+                # fetch genes count
+                result["genes_count"] = cur_query.execute((
+                    "select count(id)"
+                    " from cds"
+                    " where bgc_id=?"
+                ), (bgc_id, )).fetchall()[0][0]
+
+                # fetch class information
+                subclass_ids = [str(row[0]) for row in cur_query.execute((
+                    "select distinct chem_subclass_id"
+                    " from bgc_class"
+                    " where bgc_id=?"
+                ), (bgc_id, )).fetchall()]
+                result["classes"] = cur_source.execute((
+                    "select chem_class.name, chem_subclass.name"
+                    " from chem_subclass, chem_class"
+                    " where chem_subclass.class_id=chem_class.id"
+                    " and chem_subclass.id in ({})".format(
+                        ",".join(subclass_ids))
+                )).fetchall()
+
+    return result
+
+
+def detail_get_arrower_objects():
+    """ for arrower js """
+    result = {}
+    bgc_ids = map(int, request.args.get('bgc_id', type=str).split(","))
+    report_id = request.args.get('report_id', type=int)
+
+    db_query_path = path.join(
+        conf["reports_folder"], str(report_id), "data.db")
+
+    with sqlite3.connect(conf["db_path"]) as con_source:
+        cur_source = con_source.cursor()
+        with sqlite3.connect(db_query_path) as con_query:
+            cur_query = con_query.cursor()
+
+        for bgc_id in bgc_ids:
+            data = {}
+
+            # get bgc name, length and description
+            bgc_name, bgc_length = cur_query.execute((
+                "select bgc.name, bgc.length_nt"
+                " from bgc"
+                " where id=?"
+            ), (bgc_id, )).fetchall()[0]
+
+            data["id"] = "BGC: {}".format(bgc_name)
+            data["start"] = 0
+            data["end"] = bgc_length
+            data["desc"] = ""
+
+            # get cds
+            data["orfs"] = []
+            for cds_id, locus_tag, protein_id, \
+                    cds_start, cds_end, cds_strand in cur_query.execute((
+                        "select id, locus_tag, protein_id,"
+                        " nt_start, nt_end, strand"
+                        " from cds"
+                        " where bgc_id=?"
+                        " order by nt_start asc"
+                    ), (bgc_id, )).fetchall():
+                orf = {
+                    "start": cds_start,
+                    "end": cds_end,
+                    "strand": cds_strand
+                }
+
+                orf_names = []
+                if locus_tag:
+                    orf_names.append(locus_tag)
+                if protein_id:
+                    orf_names.append(protein_id)
+                if len(orf_names) < 1:
+                    orf_names.append("n/a")
+                orf["id"] = " / ".join(orf_names)
+
+                orf["domains"] = []
+
+                # get hsps
+                for dom_id, bitscore, dom_start, dom_end, \
+                        sub_ids, sub_scores in cur_query.execute((
+                            "select hmm_id,"
+                            " bitscore, cds_start, cds_end,"
+                            " group_concat(sub_id),"
+                            " group_concat(sub_score)"
+                            " from"
+                            " (select hsp.id as hsp_id,"
+                            " hsp.hmm_id as hmm_id,"
+                            " hsp.bitscore as bitscore,"
+                            " hsp_alignment.cds_start as cds_start,"
+                            " hsp_alignment.cds_end as cds_end,"
+                            " hspsub.hmm_id as sub_id,"
+                            " hspsub.bitscore as sub_score"
+                            " from hsp, hsp_alignment"
+                            " left join hsp_subpfam"
+                            " on hsp_subpfam.hsp_parent_id=hsp.id"
+                            " left join hsp as hspsub"
+                            " on hspsub.id=hsp_subpfam.hsp_subpfam_id"
+                            " where hsp.cds_id=?"
+                            " and hsp_alignment.hsp_id=hsp.id"
+                            " order by cds_start, hspsub.bitscore asc"
+                            ") group by hsp_id"
+                        ), (cds_id,)).fetchall():  # hsps:
+
+                    # get hmm names
+                    hmm_names = {
+                        hmm_id: name for (
+                            hmm_id, name
+                        ) in cur_source.execute((
+                            "select id, name from hmm"
+                            " where id in ({},{})"
+                        ).format(
+                            dom_id,
+                            sub_ids if sub_ids else dom_id
+                        )).fetchall()
+                    }
+
+                    dom_code = hmm_names[dom_id]
+                    if sub_ids:
+                        dom_code += " [{}]".format(
+                            ",".join([
+                                hmm_names[
+                                    int(sub_id)
+                                ].split("aligned_")[
+                                    -1
+                                ] for sub_id in sub_ids.split(",")
+                            ]))
+
+                    hsp = {
+                        "code": dom_code,
+                        "bitscore": bitscore,
+                        "start": dom_start,
+                        "end": dom_end
+                    }
+
+                    orf["domains"].append(hsp)
+
+                data["orfs"].append(orf)
+
+            # append
+            result[bgc_id] = data
+
+    return result
+
+
+def detail_get_word_cloud():
+    """ for bgc features word cloud """
+    result = {}
+    bgc_id = request.args.get('bgc_id', type=int)
+    report_id = request.args.get('report_id', type=int)
+    limit = request.args.get('limit', default=20, type=int)
+
+    db_query_path = path.join(
+        conf["reports_folder"], str(report_id), "data.db")
+
+    with sqlite3.connect(conf["db_path"]) as con_source:
+        cur_source = con_source.cursor()
+        with sqlite3.connect(db_query_path) as con_query:
+            cur_query = con_query.cursor()
+
+            result["words"] = []
+
+            # get bitscores
+            bitscores = {
+                hmm_id: list(map(float, bitscore.split(","))) for (
+                    hmm_id, bitscore
+                ) in cur_query.execute((
+                    "select hmm_id, group_concat(hsp.bitscore)"
+                    " from hsp, cds"
+                    " where hsp.cds_id=cds.id"
+                    " and cds.bgc_id=?"
+                    " group by hmm_id"
+                ), (bgc_id, )).fetchall()
+            }
+
+            # get weights
+            weights = {}
+            for subpfam_id, in cur_source.execute((
+                "select hmm_id"
+                " from subpfam"
+                " where hmm_id in ({})"
+            ).format(
+                ",".join(map(str, bitscores.keys()))
+            )).fetchall():
+                weights[subpfam_id] = sum(bitscores[subpfam_id])
+            for hmm_id in bitscores:
+                if hmm_id not in weights:
+                    weights[hmm_id] = len(bitscores[hmm_id]) * 255
+
+            # get hmm names
+            hmm_names = {
+                hmm_id: name for (
+                    hmm_id, name
+                ) in cur_source.execute((
+                    "select id, name from hmm"
+                    " where id in ({})"
+                ).format(
+                    ",".join(map(str, weights.keys()))
+                )).fetchall()
+            }
+
+            for hmm_id, weight in sorted(weights.items(), reverse=False):
+                if len(result["words"]) < limit:
+                    result["words"].append({
+                        "text": hmm_names[hmm_id],
+                        "weight": weight
+                    })
+                else:
+                    break
+
+    return result
+
+
+def detail_get_genes_table():
+    """ for genes datatable """
+    result = {}
+    result["draw"] = request.args.get('draw', type=int)
+
+    # translate request parameters
+    bgc_id = request.args.get('bgc_id', type=int)
+    report_id = request.args.get('report_id', type=int)
+    offset = request.args.get('start', type=int)
+    limit = request.args.get('length', type=int)
+
+    db_query_path = path.join(
+        conf["reports_folder"], str(report_id), "data.db")
+
+    with sqlite3.connect(conf["db_path"]) as con_source:
+        cur_source = con_source.cursor()
+        with sqlite3.connect(db_query_path) as con_query:
+            cur_query = con_query.cursor()
+
+            # fetch total records (all bgcs in the dataset)
+            result["recordsTotal"] = cur_query.execute((
+                "select count(id)"
+                " from cds"
+                " where bgc_id=?"
+            ), (bgc_id,)).fetchall()[0][0]
+
+            # fetch total records (filtered)
+            result["recordsFiltered"] = cur_query.execute((
+                "select count(id)"
+                " from cds"
+                " where bgc_id=?"
+            ), (bgc_id,)).fetchall()[0][0]
+
+            # fetch data for table
+            result["data"] = []
+            for cds_id, start, end, strand, locus_tag, \
+                    protein_id, product, aa_seq in cur_query.execute((
+                        "select id, nt_start, nt_end, strand,"
+                        " locus_tag, protein_id, product, aa_seq"
+                        " from cds"
+                        " where bgc_id=?"
+                        " order by nt_start asc"
+                        " limit ? offset ?"
+                    ), (bgc_id, limit, offset)).fetchall():
+                data = {}
+
+                # gene name
+                data["names"] = []
+                if locus_tag:
+                    data["names"].append(locus_tag)
+                if protein_id:
+                    data["names"].append(protein_id)
+                if len(data["names"]) < 1:
+                    data["names"].append("n/a")
+
+                # product
+                data["product"] = product or "n/a"
+                data["locus"] = (start, end, strand)
+                data["aa_seq"] = aa_seq
+
+                # domain information
+                data["domains"] = []
+                for dom_id, bitscore, dom_start, dom_end, \
+                        sub_ids, sub_scores in cur_query.execute((
+                            "select hmm_id,"
+                            " bitscore, cds_start, cds_end,"
+                            " group_concat(sub_id),"
+                            " group_concat(sub_score)"
+                            " from"
+                            " (select hsp.id as hsp_id,"
+                            " hsp.hmm_id as hmm_id,"
+                            " hsp.bitscore as bitscore,"
+                            " hsp_alignment.cds_start as cds_start,"
+                            " hsp_alignment.cds_end as cds_end,"
+                            " hspsub.hmm_id as sub_id,"
+                            " hspsub.bitscore as sub_score"
+                            " from hsp, hsp_alignment"
+                            " left join hsp_subpfam"
+                            " on hsp_subpfam.hsp_parent_id=hsp.id"
+                            " left join hsp as hspsub"
+                            " on hspsub.id=hsp_subpfam.hsp_subpfam_id"
+                            " where hsp.cds_id=?"
+                            " and hsp_alignment.hsp_id=hsp.id"
+                            " order by cds_start, hspsub.bitscore asc"
+                            ") group by hsp_id"
+                        ), (cds_id,)).fetchall():  # hsps:
+
+                    # get hmm names
+                    hmm_names = {
+                        hmm_id: name for (
+                            hmm_id, name
+                        ) in cur_source.execute((
+                            "select id, name from hmm"
+                            " where id in ({},{})"
+                        ).format(
+                            dom_id,
+                            sub_ids if sub_ids else dom_id
+                        )).fetchall()
+                    }
+
+                    dom_code = hmm_names[dom_id]
+                    if sub_ids:
+                        dom_code += " [{}]".format(
+                            ",".join([
+                                hmm_names[
+                                    int(sub_id)
+                                ].split("aligned_")[
+                                    -1
+                                ] for sub_id in sub_ids.split(",")
+                            ]))
+
+                    data["domains"].append([
+                        dom_code,
+                        bitscore,
+                        dom_start,
+                        dom_end
+                    ])
+
+                result["data"].append([
+                    data["names"],
+                    data["product"],
+                    data["locus"],
+                    data["domains"],
+                    (cds_id, data["aa_seq"])
+                ])
+
+    return result
 
 
 def get_overview():
@@ -286,5 +687,9 @@ routes = [
 # register api routes
 routes_api = [
     ("bgc_list", get_bgc_table),
-    ("overview", get_overview)
+    ("overview", get_overview),
+    ("detail/overview", detail_get_overview),
+    ("detail/get_arrower_objects", detail_get_arrower_objects),
+    ("detail/get_word_cloud", detail_get_word_cloud),
+    ("detail/get_genes_table", detail_get_genes_table)
 ]
