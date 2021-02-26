d64d48c8a58ce5bb4daf74f0aedf857ee00f19d4
Satria Kautsar <satriaphd@gmail.com>
2021-02-26 16:01:27 -0500

    misc: limit biopython required version for up to v1.76

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     install_requires=[
         "numpy",
         "pandas",
-        "biopython >= 1.69",
+        "biopython >= 1.73, <= 1.76",
         "scikit-learn",
         "pysqlite3"
     ]
