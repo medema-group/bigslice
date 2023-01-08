24815ead0af97478dfdef21d20206cca1b1d1051
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-08 20:19:43 +0000

    Adding tarfile member sanitization to extractall()

--- bigslice/db/advanced/generate_databases.py
+++ bigslice/db/advanced/generate_databases.py
@@ -90,7 +90,29 @@ def main():
 
             print("Extracting antismash.tar.gz...")
             with tarfile.open(antismash_zipped_file, "r:gz") as as_zipped:
-                as_zipped.extractall(path=antismash_folder)
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner) 
+                    
+                
+                safe_extract(as_zipped, path=antismash_folder)
                 subprocess.run("mv {} {}".format(
                     path.join(antismash_folder, "*/*"),
                     antismash_folder
